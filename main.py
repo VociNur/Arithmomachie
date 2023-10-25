406591823629408c36718c680b3e973205020bee
VociNur <vocijules@gmail.com>
2023-10-25 16:46:12 +0200

    Partie jusqu’à la première attaque mélée #1

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 from tkinter import *
 import numpy as np
 import json
@@ -13,7 +14,7 @@ def print_board(board):
 
 
 class Game:
-    #Permet de visualiser le jeu à la fin de la partie
+    # Permet de visualiser le jeu à la fin de la partie
     def show_game(self):
         tk = Tk()
         tk.config(width=400, height=800)
@@ -25,22 +26,23 @@ class Game:
             for j in range(self.height):
                 if not self.is_empty(j, i):
                     (point, form, team) = self.board[j][i]
-                    color = "Blue" if team==0 else "Red"
+                    color = "Blue" if team == 0 else "Red"
                     if form == 1:
-                        canvas.create_oval(i*50 + 5, j*50 + 5, (i+1)*50 - 5, (j+1)*50-5, outline=color, fill="WHITE", width=2)
+                        canvas.create_oval(i * 50 + 5, j * 50 + 5, (i + 1) * 50 - 5, (j + 1) * 50 - 5, outline=color,
+                                           fill="WHITE", width=2)
                     if form == 2:
                         dp = np.array([(-20, 20), (20, 20), (0, -20)])
-                        points = (i*50+25, j*50+25) + dp
+                        points = (i * 50 + 25, j * 50 + 25) + dp
                         canvas.create_polygon(points.flatten().tolist(), outline=color, fill="WHITE", width=2)
                     if form == 3:
-                        canvas.create_rectangle(i*50 + 5, j*50 + 5, (i+1)*50 - 5, (j+1)*50-5, outline=color, fill="WHITE", width=2)
+                        canvas.create_rectangle(i * 50 + 5, j * 50 + 5, (i + 1) * 50 - 5, (j + 1) * 50 - 5,
+                                                outline=color, fill="WHITE", width=2)
 
-                    canvas.create_text(i*50+25, j*50+25, text=str(point))
+                    canvas.create_text(i * 50 + 25, j * 50 + 25, text=str(point))
 
         canvas.pack()
 
         mainloop()
-
 
     def add_piece(self, y, x, side, form, value):
         self.board[y][x] = (side, form, value)
@@ -50,15 +52,15 @@ class Game:
         self.board = np.array(json.loads(f.read()))
         f.close()
 
-    #Vérifie si la position est bien dans le jeu
+    # Vérifie si la position est bien dans le jeu
     def in_board(self, j, i):
         return 0 <= j < self.height and 0 <= i < self.width
 
-    #Vérifie si une case est libre, sans pion
+    # Vérifie si une case est libre, sans pion
     def is_empty(self, j, i):
         return np.equal(self.board[j][i], [-1, -1, -1]).all()
 
-    #On récupère les mouvements réguliers, on doit vérifier que tout le trajet est libre
+    # On récupère les mouvements réguliers, on doit vérifier que tout le trajet est libre
     def get_pawn_available_regular_moves(self, pawn, j, i):
         available_moves = []
         (value, form, team) = pawn
@@ -97,7 +99,7 @@ class Game:
 
         return available_moves
 
-    #On récupère les mouvements irréguliers, juste à vérifier que la case finale est libre
+    # On récupère les mouvements irréguliers, juste à vérifier que la case finale est libre
     def get_pawn_available_irregular_moves(self, pawn, j, i):
         available_moves = []
         (value, form, team) = pawn
@@ -126,8 +128,8 @@ class Game:
 
         return available_moves
 
-    #récupère tous les mouvements possibles dans un état
-    #Pour chaque cases, si elle n’est pas vide, on récupère les mouvements régulier et irrégulier
+    # récupère tous les mouvements possibles dans un état
+    # Pour chaque cases, si elle n’est pas vide, on récupère les mouvements régulier et irrégulier
     def get_game_available_moves(self):
         available_moves = []  # 1 move = 2 couples (y, x)
         # pour toutes les cases non vide, on ajoute ses coups possible dans la liste des coups
@@ -141,66 +143,87 @@ class Game:
                     available_moves += self.get_pawn_available_irregular_moves((value, form, team), y, x)
         return available_moves
 
-    #permet de délpacer une pièce
+    # permet de délpacer une pièce
     def move(self, move):
         ((y, x), (dy, dx)) = move
         self.board[y + dy][x + dx] = self.board[y][x]
         self.board[y][x] = (-1, -1, -1)
         self.turn += 1
-        self.player_turn = (self.player_turn + 1)%2
+        self.player_turn = (self.player_turn + 1) % 2
 
-    def get_pawn_melee_attacks(self, pawn, j, i): #copire coller de get_pawn_available_moves mais on veut un pion dans les coins
+    def get_pawn_melee_attacks(self, pawn, j,
+                               i):  # copire coller de get_pawn_available_moves mais on veut un pion dans les coins
         available_attacks = []
         (value, form, team) = pawn
         if form == 1 or form == 4:  # c’est un rond
             relative_moves_circle = [(1, 1), (1, -1), (-1, 1), (-1, -1)]
             for rm in relative_moves_circle:
                 dj, di = rm
-                if self.in_board(j + dj, i + di) and not self.is_empty(j+dj, i+di) \
-                        and self.board[j + dj][i + di][2] != self.player_turn: #ici on inverse
+                if self.in_board(j + dj, i + di) and not self.is_empty(j + dj, i + di) \
+                        and self.board[j + dj][i + di][2] != self.player_turn:  # ici on inverse
                     available_attacks += [(j + dj, i + di)]
 
         if form == 2 or form == 4:
             range = 2
-            space = range-1#j’enlève 1 à chaque fois et on vérifie qu’il y a bien quelque chose en "r+1"
-            if self.in_board(j + range, i) and np.equal(self.board[j + 1:j + space + 1, i], np.full((space, 3), -1)).all() \
-                    and (not self.is_empty(j+range, i)) and self.board[j + range][i][2] != self.player_turn:
+            space = range - 1  # j’enlève 1 à chaque fois et on vérifie qu’il y a bien quelque chose en "r+1"
+            if self.in_board(j + range, i) \
+                    and np.equal(self.board[j + 1:j + space + 1, i], np.full((space, 3), -1)).all() \
+                    and (not self.is_empty(j + range, i)) \
+                    and self.board[j + range][i][2] != self.player_turn:
                 available_attacks += [(j + range, i)]
 
-            if self.in_board(j - range, i) and np.equal(self.board[j - space:j, i], np.full((space, 3), -1)).all() \
-                    and (not self.is_empty(j-range, i)) and self.board[j - range][i][2] != self.player_turn:
+            if self.in_board(j - range, i) \
+                    and np.equal(self.board[j - space:j, i], np.full((space, 3), -1)).all() \
+                    and (not self.is_empty(j - range, i)) \
+                    and self.board[j - range][i][2] != self.player_turn:
                 available_attacks += [(j - range, i)]
 
-            if self.in_board(j, i + range) and np.equal(self.board[j, i + 1:i + space + 1], np.full((space, 3), -1)).all() \
-                    and (not self.is_empty(j, i+range)) and self.board[j][i + range][2] != self.player_turn:
+            if self.in_board(j, i + range) \
+                    and np.equal(self.board[j, i + 1:i + space + 1], np.full((space, 3), -1)).all() \
+                    and (not self.is_empty(j, i + range)) \
+                    and self.board[j][i + range][2] != self.player_turn:
                 available_attacks += [(j, i + range)]
 
-            if self.in_board(j, i - range) and np.equal(self.board[j, i - space:i], np.full((space, 3), -1)).all() \
-                    and (not self.is_empty(j, i-range)) and self.board[j][i - range][2] != self.player_turn:
+            if self.in_board(j, i - range) \
+                    and np.equal(self.board[j, i - space:i], np.full((space, 3), -1)).all() \
+                    and (not self.is_empty(j, i - range)) \
+                    and self.board[j][i - range][2] != self.player_turn:
                 available_attacks += [(j, i - range)]
 
         if form == 3 or form == 4:
             range = 3
-            space = range-1
-            if self.in_board(j + range, i) and np.equal(self.board[j + 1:j + space + 1, i], np.full((space, 3), -1)).all() \
-                    and (not self.is_empty(j+range, i)) and self.board[j + range][i][2] != self.player_turn:
+            space = range - 1
+            if self.in_board(j + range, i) \
+                    and np.equal(self.board[j + 1:j + space + 1, i], np.full((space, 3), -1)).all() \
+                    and (not self.is_empty(j + range, i)) \
+                    and self.board[j + range][i][2] != self.player_turn:
                 available_attacks += [(j + range, i)]
 
-            if self.in_board(j - range, i) and np.equal(self.board[j - space:j, i], np.full((space, 3), -1)).all() \
-                    and (not self.is_empty(j-range, i)) and self.board[j - range][i][2] != self.player_turn:
+            if self.in_board(j - range, i) \
+                    and np.equal(self.board[j - space:j, i], np.full((space, 3), -1)).all() \
+                    and (not self.is_empty(j - range, i)) \
+                    and self.board[j - range][i][2] != self.player_turn:
                 available_attacks += [(j - range, i)]
 
-            if self.in_board(j, i + range) and np.equal(self.board[j, i + 1:i + space + 1], np.full((space, 3), -1)).all() \
-                    and (not self.is_empty(j, i+range)) and self.board[j][i + range][2] != self.player_turn:
+            if self.in_board(j, i + range) \
+                    and np.equal(self.board[j, i + 1:i + space + 1], np.full((space, 3), -1)).all() \
+                    and (not self.is_empty(j, i + range)) \
+                    and self.board[j][i + range][2] != self.player_turn:
                 available_attacks += [(j, i + range)]
 
-            if self.in_board(j, i - range) and np.equal(self.board[j, i - space:i], np.full((space, 3), -1)).all() \
-                    and (not self.is_empty(j, i-range)) and self.board[j][i - range][2] != self.player_turn:
+            if self.in_board(j, i - range) \
+                    and np.equal(self.board[j, i - space:i], np.full((space, 3), -1)).all() \
+                    and (not self.is_empty(j, i - range)) \
+                    and self.board[j][i - range][2] != self.player_turn:
                 available_attacks += [(j, i - range)]
 
         return available_attacks
-    def get_melee_attack(self):
+
+    def get_melee_attack_board(self):
         attack_board = np.full((16, 8), list)
+        for y in range(16):
+            for x in range(8):
+                attack_board[y][x] = []
         for y in range(16):
             for x in range(8):
                 if self.is_empty(y, x):
@@ -209,16 +232,43 @@ class Game:
                 if team == self.player_turn:
                     for attack in self.get_pawn_melee_attacks((value, form, team), y, x):
                         print("attack", attack, value)
+                        attack_board[attack[0]][attack[1]].append(value)
         return attack_board
 
+    def get_melee_attack(self):
+        attacked = []
+        attack_board = self.get_melee_attack_board()
+        for y in range(16):
+            for x in range(8):
+                attackers = attack_board[y][x]
 
+
+                #Rencontre et puissance
+                for a in attackers:
+                    if (math.log(self.board[y][x][0])/math.log(a)).is_integer() or (math.log(a) / math.log(self.board[y][x][0])).is_integer():
+                        #print("démooooo rencontre ou pui", y, x, ":", self.board[y][x][0], "by", a)
+                        attacked.append((y, x))
+                #Embuscade
+                for i in range(len(attackers)):
+                    for j in range(i):
+                        if attackers[i] + attackers[j] == self.board[y][x][0] \
+                                or abs(attackers[i] - attackers[j]) == self.board[y][x][0] \
+                                or attackers[i] * attackers[j] == self.board[y][x][0] \
+                                or attackers[i] / attackers[j] == self.board[y][x][0] \
+                                or attackers[j] / attackers[i] == self.board[y][x][0]:
+                            #print("defouuuuu", y, x, ":", self.board[y][x][0], "by", attackers[i], attackers[j])
+                            attacked.append((y, x))
+
+
+
+        return attacked
     def get_game_available_attack(self):
         available_attacks = []
-
+        available_attacks += self.get_melee_attack()
         return available_attacks
 
     def __init__(self):
-        self.board = []
+        self.board = [] #valeur forme, équipe
         self.init_board()
         self.turn = 0  # numéro du tour
         self.player_turn = 0  # numéro de celui à jouer
@@ -226,16 +276,14 @@ class Game:
         self.height = 16
 
         print("test")
-        for i in range(1000):
-            coups = self.get_game_available_moves()
-            #print(len(coups), coups)
-            # print("--")
-            coup = coups[random.randint(0, len(coups) - 1)]
-            # print("jouer", coup)
-            self.move(coup)
 
+
+        while not self.get_game_available_attack():
+            coups = self.get_game_available_moves()
+            coup = coups[random.randint(0, len(coups) - 1)]
+            self.move(coup)
         print_board(self.board)
-        self.get_melee_attack()
+        print(self.get_game_available_attack())
         self.show_game()
         print("ek")
 
