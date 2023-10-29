1792796302bb0c3518ae9c09a9fb04d472744b22
VociNur <vocijules@gmail.com>
2023-10-29 16:41:33 +0100

    Attaque progression #9 +Modif affichage

--- enums/type_attack.py
+++ enums/type_attack.py
@@ -2,9 +2,11 @@ from enum import Enum
 
 
 class TypeAttack(Enum):
-    MEET = "-M"
-    GALLOWS = "-G"
-    AMBUSH = "-AM"
-    ASSAULT = "-AS"
-    SIEGE = "-S"
-
+    MEET = "-M"  # mélée
+    GALLOWS = "-G"  # melée
+    AMBUSH = "-AM"  # melée
+    PROGRESSION_A = "-PA"  # melée
+    PROGRESSION_G = "-PG"  # melée
+    PROGRESSION_H = "-PH"  # melée
+    ASSAULT = "-AS"  # de loin
+    SIEGE = "-S"  # aucun des deux
